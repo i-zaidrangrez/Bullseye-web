fe30d61b211fb7bac5ee29b5f5a4365971c47b5f
Zaid Rangrez <zaidrangrez.me@gmail.com>
2026-06-19 19:59:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Hero from './Component/Hero'
+
+
+const App = () => {
+  return (
+    <div className='h-screen'>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
